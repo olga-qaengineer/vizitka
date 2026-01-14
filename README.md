97edc38a66268ee936735710ec22696969883fde
olga-qaengineer <olgaboyarock@yahoo.com>
2026-01-14 15:27:31 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# vizitka
-Сайт визитка на html и css
+# cv_test
+Сайт-визика на HTML + CSS + JS
---
+++ js.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('button');
+
+button.addEventListener('click', function() {
+	alert('Я изучила ваш продукт и уже вижу несколько векторов для улучшения его стабильности, о которых готова рассказать подробнее')
+})
